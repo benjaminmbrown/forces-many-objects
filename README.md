9ef15f7daea626a75665cf7c96a19f10b5407b65
Benjamin M. Brown <benjaminbb@gmail.com>
2016-04-09 14:54:31 -0500

    making forces work for many objects and creating instances of many objects with varying masses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # forces-many-objects
 Example of applying additive forces to many objects of varied mass in Processing
+<img src="https://media.giphy.com/media/3o85g1CkOExXBZqPvi/giphy.gif"/>
---
+++ sketch.js
@@ -0,0 +1,34 @@
+var movers=[];
+var noiseScale = 0.001;
+
+function setup() {
+  createCanvas(440, 260);
+  //m = new Mover(random(0.1, 5), 0, 0)
+
+    for (var i = 0; i < 30; i++) {
+      movers[i] = new Mover(random(0.1,5),0,0);
+  }
+}
+
+function draw() {
+  background(51);
+  //noise() takes  params value, valRangeMin, valRangeMax, desiredRangeMin,desiredRangeMax
+  var wind = createVector(0.01, 0);
+  var gravity = createVector(0, 0.1);
+
+  for (var i = 0; i < movers.length; i++) {
+     movers[i].applyForce(wind);
+     movers[i].applyForce(gravity);
+     movers[i].update();
+     movers[i].display();
+     movers[i].checkEdges();
+
+  }
+  
+
+noiseScale+=0.001;
+
+
+}
+
+
